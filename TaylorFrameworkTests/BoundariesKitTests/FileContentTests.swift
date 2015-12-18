84f19536f483f53196c8442b33f05bc9563a5d33
Dan Ursu <dan.ursu@yopeso.com>
2015-12-19 00:40:39 +0100

    Delete unit test to check that codecov.io reports this change

--- TaylorFrameworkTests/BoundariesKitTests/FileContentTests.swift
+++ TaylorFrameworkTests/BoundariesKitTests/FileContentTests.swift
@@ -12,28 +12,28 @@ import Nimble
 @testable import TaylorFramework
 
 class FileContentTests: QuickSpec {
-    override func spec() {
-        
-        describe("File Content") {
-            
-            let path = "path/to/tests"
-            let content = FileContent(path: path, components: [])
-
-            it("should contain path and components") {
-                expect(content.path).to(equal(path))
-                expect(content.components).to(equal([]))
-            }
-            
-            it("should be equal to a identical file content") {
-                let identicalContent = FileContent(path: path, components: [])
-                expect(content).to(equal(identicalContent))
-            }
-            
-            it("should not be equal to a different file content") {
-                let differentContent = FileContent(path: "different/path", components: [])
-                expect(content).notTo(equal(differentContent))
-            }
-        }
-    }
+//    override func spec() {
+//        
+//        describe("File Content") {
+//            
+//            let path = "path/to/tests"
+//            let content = FileContent(path: path, components: [])
+//
+//            it("should contain path and components") {
+//                expect(content.path).to(equal(path))
+//                expect(content.components).to(equal([]))
+//            }
+//            
+//            it("should be equal to a identical file content") {
+//                let identicalContent = FileContent(path: path, components: [])
+//                expect(content).to(equal(identicalContent))
+//            }
+//            
+//            it("should not be equal to a different file content") {
+//                let differentContent = FileContent(path: "different/path", components: [])
+//                expect(content).notTo(equal(differentContent))
+//            }
+//        }
+//    }
     
 }
